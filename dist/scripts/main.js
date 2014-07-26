36e502fd5d57da123554d865acf78bb5828c92d5
maswhitaker <masforwhi@yahoo.com>
2014-07-26 01:01:03 -0400

    Implemented backbone event

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -6,6 +6,10 @@
   {name:'dolphin',imgurl:'../images/dolphin.png',age: 8,bio:'dolphy dolphy!! eek eek!'},
   {name:'Yellowfish',imgurl:'../images/yellowfish.png',age: 10,bio:'a yellow goldfish'}];
 
+  var events = [{eventname:'Kids Day'},
+  {eventname:'Non-Kids Day'},
+  {eventname:'beeeep'}];
+
 
   var Animal = Backbone.Model.extend({
     defaults: {
@@ -99,15 +103,75 @@
   });
 
 
+var GalleryModel = Backbone.Model.extend({
+  defaults:{
+    eventname: ''
+  }
+});
+
+var GalleryCollection = Backbone.Collection.extend({
+  model: GalleryModel
+});
+
+var GalleryCollectionView = Backbone.View.extend({
+  el:$('#events'),
+  initialize: function(){
+    this.collection = new GalleryCollection(events);
+    this.render();
+  },
+  render: function(){
+    var that = this;
+    _.each(this.collection.models,function(item){
+      that.renderEvent(item);
+    },this);
+  },
+  renderEvent: function(item){
+    var galleryView = new GalleryView({
+      model: item
+    });
+    this.$el.append(galleryView.render().el);
+  }
+});
+
+var GalleryView = Backbone.View.extend({
+  tagName: 'li',
+  className: 'gallery',
+  template: $('#event-template').html(),
+  render: function(){
+    var tmpl = _.template(this.template);
+    this.$el.html(tmpl(this.model.toJSON()));
+    return this;
+  }
+});
+
+
+
+
+
+
+
+
 var FishRouter = Backbone.Router.extend({
     routes: {
+        '': 'home',
         "fish": "fish",
         "rare-fish": "rare-fish"
     }
 });
 
+$('#click').click(function(){
+  var galleryCollectionView = new GalleryCollectionView();
+});
+
+$('#hide').click(function(){
+  location.reload();
+});
 
 var app_router = new FishRouter();
+
+app_router.on('route:home',function(){
+
+});
 
 app_router.on('route:fish', function () {
       $('.fishContainer').css('display', 'none');
@@ -115,6 +179,7 @@ app_router.on('route:fish', function () {
       var animalCollectionView = new AnimalCollectionView();
 
 });
+
 app_router.on('route:rare-fish', function () {
       $('.animalContainer').css('display', 'none');
       $('.fishContainer').css('display', 'initial');
